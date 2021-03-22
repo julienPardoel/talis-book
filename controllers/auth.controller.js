681809331d93fb2a9bdd18fced7c4a0a620c133b
DESKTOP-189F2TD\jux33 <julien.pardoel@gmail.com>
2021-03-22 15:49:21 +0100

    bcrypt password ok

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,15 @@
-const UserModel = require ('../models/user.model');
+const UserModel = require('../models/user.model');
 
 module.exports.signUp = async (req, res) => {
 
     console.log(req.body);
 
-    const {pseudo, email, password} = req.body;
+    const { pseudo, email, password } = req.body;
 
     try {
-        const user = await UserModel.create({pseudo, email, password});
+        const user = await UserModel.create({ pseudo, email, password });
         res.status(201).json({ user: user._id })
-    } catch(err) {
+    } catch (err) {
         res.status(200).send({ err })
     }
 }
--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require ('mongoose');
-const { isEmail } = require('validator')
+const { isEmail } = require('validator');
+const bcrypt = require ('bcrypt')
 
 const userSchema = new mongoose.Schema(
     {
@@ -46,6 +47,12 @@ const userSchema = new mongoose.Schema(
         timestamps: true
     }
 )
+
+userSchema.pre("save", async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
 
 const UserModel = mongoose.model('user', userSchema);
 
