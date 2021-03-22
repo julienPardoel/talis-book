417bd7f26978a4fa89c9f5864ea7a711886853ee
DESKTOP-189F2TD\jux33 <julien.pardoel@gmail.com>
2021-03-22 16:30:35 +0100

    add updateUser

---
+++ controllers/user.controller.js
@@ -0,0 +1,41 @@
+const UserModel = require('../models/user.model');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+module.exports.getAllUsers = async (req, res) => {
+    const users = await UserModel.find().select('-password');
+    res.status(200).json(users);
+}
+
+module.exports.userInfo = async (req, res) => {
+    console.log(req.params);
+    if (!ObjectId.isValid(req.params.id))
+        return res.staus(400).send('ID unknow : ' + req.params.id)
+
+    UserModel.findById(req.params.id, (err, docs) => {
+        if (!err) res.send(docs);
+        else console.log('ID unknow : ' + err);
+    }).select('-password')
+}
+
+module.exports.updateUser = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.staus(400).send('ID unknown : ' + req.params.id)
+
+    try {
+        await UserModel.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $set: {
+                    bio: req.body.bio
+                }
+            },
+            { new: true, upsert: true, setDefaultsOnInsert: true},
+            (err, docs) => {
+                if (!err) return res.send(docs);
+                if (err) return res.status(500).send({message: err});
+            }
+        )
+    } catch (err) {
+        return res.status(500).json({ message: err });
+    }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const authController = require ('../controllers/auth.controller');
+const userController = require ('../controllers/user.controller');
 
 router.post("/register", authController.signUp);
+
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.userInfo);
+router.put('/:id', userController.updateUser);
 
 module.exports = router;
