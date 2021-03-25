1f10c3562128fd9bfacad0011e441e7c7b1cf4bf
DESKTOP-189F2TD\jux33 <julien.pardoel@gmail.com>
2021-03-25 09:54:29 +0100

    add post comment and edit comment

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -12,7 +12,8 @@ module.exports.readPost = (req, res) => {
     if (!err) res.send(docs);
     else console.log("Error to get data : " + err);
   })
-  // .sort({ createdAt: -1 });
+  // la ligne suivante permet d'afficher le message le plus recent en premier
+  .sort({ createdAt: -1 });
 };
 
 module.exports.createPost = async (req, res) => {
@@ -150,78 +151,78 @@ module.exports.unlikePost = async (req, res) => {
   }
 };
 
-// module.exports.commentPost = (req, res) => {
-//   // if (!ObjectID.isValid(req.params.id))
-//   //   return res.status(400).send("ID unknown : " + req.params.id);
+module.exports.commentPost = (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
 
-//   // try {
-//   //   return PostModel.findByIdAndUpdate(
-//   //     req.params.id,
-//   //     {
-//   //       $push: {
-//   //         comments: {
-//   //           commenterId: req.body.commenterId,
-//   //           commenterPseudo: req.body.commenterPseudo,
-//   //           text: req.body.text,
-//   //           timestamp: new Date().getTime(),
-//   //         },
-//   //       },
-//   //     },
-//   //     { new: true },
-//   //     (err, docs) => {
-//   //       if (!err) return res.send(docs);
-//   //       else return res.status(400).send(err);
-//   //     }
-//   //   );
-//   // } catch (err) {
-//   //   return res.status(400).send(err);
-//   // }
-// };
+  try {
+    return PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          comments: {
+            commenterId: req.body.commenterId,
+            commenterPseudo: req.body.commenterPseudo,
+            text: req.body.text,
+            timestamp: new Date().getTime(),
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
 
-// module.exports.editCommentPost = (req, res) => {
-//   // if (!ObjectID.isValid(req.params.id))
-//   //   return res.status(400).send("ID unknown : " + req.params.id);
+module.exports.editCommentPost = (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
 
-//   // try {
-//   //   return PostModel.findById(req.params.id, (err, docs) => {
-//   //     const theComment = docs.comments.find((comment) =>
-//   //       comment._id.equals(req.body.commentId)
-//   //     );
+  try {
+    return PostModel.findById(req.params.id, (err, docs) => {
+      const theComment = docs.comments.find((comment) =>
+        comment._id.equals(req.body.commentId)
+      );
 
-//   //     if (!theComment) return res.status(404).send("Comment not found");
-//   //     theComment.text = req.body.text;
+      if (!theComment) return res.status(404).send("Comment not found");
+      theComment.text = req.body.text;
 
-//   //     return docs.save((err) => {
-//   //       if (!err) return res.status(200).send(docs);
-//   //       return res.status(500).send(err);
-//   //     });
-//   //   });
-//   // } catch (err) {
-//   //   return res.status(400).send(err);
-//   // }
-// };
+      return docs.save((err) => {
+        if (!err) return res.status(200).send(docs);
+        return res.status(500).send(err);
+      });
+    });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
 
-// module.exports.deleteCommentPost = (req, res) => {
-//   // if (!ObjectID.isValid(req.params.id))
-//   //   return res.status(400).send("ID unknown : " + req.params.id);
+module.exports.deleteCommentPost = (req, res) => {
+  // if (!ObjectID.isValid(req.params.id))
+  //   return res.status(400).send("ID unknown : " + req.params.id);
 
-//   // try {
-//   //   return PostModel.findByIdAndUpdate(
-//   //     req.params.id,
-//   //     {
-//   //       $pull: {
-//   //         comments: {
-//   //           _id: req.body.commentId,
-//   //         },
-//   //       },
-//   //     },
-//   //     { new: true },
-//   //     (err, docs) => {
-//   //       if (!err) return res.send(docs);
-//   //       else return res.status(400).send(err);
-//   //     }
-//   //   );
-//   // } catch (err) {
-//   //   return res.status(400).send(err);
-//   // }
-// };
+  // try {
+  //   return PostModel.findByIdAndUpdate(
+  //     req.params.id,
+  //     {
+  //       $pull: {
+  //         comments: {
+  //           _id: req.body.commentId,
+  //         },
+  //       },
+  //     },
+  //     { new: true },
+  //     (err, docs) => {
+  //       if (!err) return res.send(docs);
+  //       else return res.status(400).send(err);
+  //     }
+  //   );
+  // } catch (err) {
+  //   return res.status(400).send(err);
+  // }
+};
