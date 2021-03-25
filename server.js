4e5c4714701fb405dbf7013a79720ec4aebbc204
DESKTOP-189F2TD\jux33 <julien.pardoel@gmail.com>
2021-03-25 09:18:45 +0100

    add post create update delete

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
+const postRoutes = require('./routes/post.routes');
 require('dotenv').config({ path: './config/.env' });
 require('./config/db');
 const { checkUser, requireAuth } = require('./middleware/auth.middleware');
@@ -11,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-//jwt
+//jsonwebtoken
 app.get('*', checkUser);
 app.get('/jwtid', requireAuth, (req, res) => {
     res.status(200).send(res.locals.user._id)
@@ -19,6 +20,7 @@ app.get('/jwtid', requireAuth, (req, res) => {
 
 //routes
 app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
 
 //server
 app.listen(process.env.PORT, () => {
