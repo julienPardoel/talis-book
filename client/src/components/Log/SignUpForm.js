39be44a60b33cb85a9625680048430c95ab67a96
DESKTOP-189F2TD\jux33 <julien.pardoel@gmail.com>
2021-03-26 15:39:04 +0100

    front signin

---
+++ client/src/components/Log/SignUpForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUpForm = () => {
+  return (
+    <div>
+      connecter
+    </div>
+  )
+}
+
+export default SignUpForm
---
+++ client/src/pages/Profil.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Log from "../components/Log";
+// import { UidContext } from "../components/AppContext";
+// import UpdateProfil from "../components/Profil/UpdateProfil";
+
+
+const Profil = () => {
+    return (
+        <div className="profil-page">
+            <div className="log-container">
+                <Log signin={false} signup={true} />
+                <div className="img-container">
+                    <img src="./img/log.svg" alt="img-log" />
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profil
