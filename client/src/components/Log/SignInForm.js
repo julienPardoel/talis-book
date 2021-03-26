53b383cf8bc054bae765298a494bf58074a2cf19
DESKTOP-189F2TD\jux33 <julien.pardoel@gmail.com>
2021-03-26 18:26:52 +0100

    front signin v2

--- client/src/components/Log/SignInForm.js
+++ client/src/components/Log/SignInForm.js
@@ -1,24 +1,3 @@
-// import React from 'react'
-
-// const SignInForm = () => {
-//   return (
-//     <div>
-//       inscription
-//     </div>
-//   )
-// }
-
-// export default SignInForm
-
-
-
-
-
-
-
-
-
-
 import React, { useState } from "react";
 import axios from "axios";
 
@@ -27,7 +6,31 @@ const SignInForm = () => {
   const [password, setPassword] = useState("");
 
   const handleLogin = (e) => {
-    
+        e.preventDefault();
+        const emailError = document.querySelector(".email.error");
+        const passwordError = document.querySelector(".password.error");
+
+        axios({
+          method: "post",
+          url: `${process.env.REACT_APP_API_URL}api/user/login`,
+          withCredentials: true,
+          data: {
+            email,
+            password,
+          },
+        })
+          .then((res) => {
+            console.log(res);
+            if (res.data.errors) {
+              emailError.innerHTML = res.data.errors.email;
+              passwordError.innerHTML = res.data.errors.password;
+            } else {
+              window.location = "/";
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
   };
 
   return (
--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Log from "../components/Log";
-// import { UidContext } from "../components/AppContext";
+import { UidContext } from "../components/AppContext";
 // import UpdateProfil from "../components/Profil/UpdateProfil";
 
 
 const Profil = () => {
+    const uid = useContext(UidContext);
+
     return (
         <div className="profil-page">
-            <div className="log-container">
-                <Log signin={false} signup={true} />
-                <div className="img-container">
-                    <img src="./img/log.svg" alt="img-log" />
-
+            {uid ? (
+                // <UpdateProfil />
+                <h1>UPDATE PAGE</h1>
+            ) : (
+                <div className="log-container">
+                    <Log signin={false} signup={true} />
+                    <div className="img-container">
+                        <img src="./img/log.svg" alt="img-log" />
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
