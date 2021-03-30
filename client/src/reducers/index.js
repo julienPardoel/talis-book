9d1207bff57b55bb04afe5d12c4ae3382ca98cba
DESKTOP-189F2TD\jux33 <julien.pardoel@gmail.com>
2021-03-30 16:24:48 +0200

    logique followhandler post

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import userReducer from './user.reducer';
 import usersReducer from './users.reducer';
-// import postReducer from './post.reducer';
+import postReducer from './post.reducer';
 // import errorReducer from './error.reducer';
 // import allPostsReducer from './allPosts.reducer';
 // import trendingReducer from './trending.reducer';
@@ -9,7 +9,7 @@ import usersReducer from './users.reducer';
 export default combineReducers({
   userReducer,
   usersReducer,
-//   postReducer,
+  postReducer,
 //   errorReducer,
 //   allPostsReducer,
 //   trendingReducer
